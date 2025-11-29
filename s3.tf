09a0a1f5a6adfbb6c924345d2cb4218dd9ec19c8
katehjd21 <katedrew2103@hotmail.co.uk>
2025-11-29 08:07:51 +0000

    changing state lock file name

--- s3.tf
+++ s3.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "kd-testing-pyramid-s3-bucket"
     key            = "testing-pyramid/terraform.tfstate"
     region         = "eu-west-2"
-    dynamodb_table = "terraform-state-lock"
+    dynamodb_table = "kd-terraform-state-lock"
     encrypt        = true
   }
 }
